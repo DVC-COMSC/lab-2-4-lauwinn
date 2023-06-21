4721583919e994ed7bbd851db7943ca454249868
Winnie <lau.winnies@gmail.com>
2023-06-20 22:17:51 -0700

    Ch 2 Lab 4 String

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+
+    original_str = "Python Programming"
+    sub1 = original_str.split()[1]
+    sub2 = original_str[:-11]
+    merged_str = sub1 + ' ' + sub2
+    print(sub1)
+    print(sub2)
+    print(merged_str)
+
     pass
 
 
